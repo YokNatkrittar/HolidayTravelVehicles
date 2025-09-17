3e5ed6210eea483794833f419f0104c82649f69a
Yok <yokojournal.co@gmail.com>
2025-09-17 14:45:02 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Vehicle newVehicle = new Vehicle(00001, "Honda", "CR-V", 2025, "Honda",  28410);
+        System.out.println(newVehicle);
+    }
+}
